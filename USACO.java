4555f9fe5aac81464ba77f1b2e30c747ea57a296
almathaler <athaler00@stuy.edu>
2019-03-09 21:35:35 -0500

    might work not sure yet

--- USACO.java
+++ USACO.java
@@ -6,7 +6,7 @@ public class USACO{
 
   public static void main(String[] args){
     try{
-      silver("ctravel.1.in");
+      System.out.println(silver("ctravel.1.in"));
     }catch(FileNotFoundException e){
       //System.out.println("file not here");
     }
@@ -88,23 +88,105 @@ public class USACO{
     int time = in.nextInt();
     in.nextLine();//idk why but need to move cursor down
     char[][] boardChars = new char[rows][cols];
-    for (int i = 0; i<rows; i++){
+    boolean[][] canUpdate = new boolean[rows][cols];
+    for (int rr = 0; rr<rows; rr++){
       String process = in.nextLine();
       //System.out.println(process);
-      board[i] = process.toCharArray();
+      boardChars[rr] = process.toCharArray();
+      for (int cc = 0; cc<cols; cc++){ //setting up canUpdate to mean only non-trees can update
+        if (boardChars[rr][cc] != '*'){
+          canUpdate[rr][cc] = true;
+        }else{
+          canUpdate[rr][cc] = false;
+        }
+      }
     }
-    int rowStart = in.nextInt();
-    int colStart = in.nextInt();
-    int rowEnd = in.nextInt();
-    int colEnd = in.nextInt();
-    //make board of booleans
-    //make n = 0 board [EVEN]
-    //make n = 1 board [ODD]
-    //make workingEven board and workingOdd board
-    //workingEven updated to be n=2, will be all the ok-for-even-to-update squares
-    //updated as the sum of the values of the 4 ok (boolean board determines if it's ok)
-    //values around them
-    //n = 0 determined from start, just a 1 at the starting position
-    return -1;
+    int rowStart = in.nextInt() - 1;
+    int colStart = in.nextInt() - 1;
+    int rowEnd = in.nextInt() - 1;
+    int colEnd = in.nextInt() - 1;
+    int[][] boardA = new int[rows][cols];
+    int[][] boardB = new int[rows][cols];
+    for (int row = 0; row<rows; row++){
+      for (int col = 0; col<cols; col++){
+        boardA[row][col] = 0;
+        boardB[row][col] = 0;
+        if (row == rowStart && col == colStart){
+          boardA[row][col] = 1;
+        }
+      }
+    }//initialized boards
+    int toReturn = boardA[rowEnd][colEnd]; // this is the val of toReturn
+    for (int curTime = 1; curTime <= time; curTime++){//idt it makes a difference to count back, if time is 0 won't enter loop otherwise makes no diff
+      if (curTime%2 == 0){ //modify boardA
+        System.out.println("\n" + curTime + "printing boardA");
+        //System.out.println(Arrays.toString(boardA));
+        for (int r = 0; r<rows; r++){
+          System.out.println(Arrays.toString(boardA[r]));
+          for (int c = 0; c<cols; c++){
+            if (boardB[r][c] == 0 && canUpdate[r][c]){ //means can be modified in other board
+              try{
+                boardA[r][c] += boardB[r+1][c];
+              }catch (IndexOutOfBoundsException e){
+
+              }try{
+                boardA[r][c] += boardB[r-1][c];
+              }catch (IndexOutOfBoundsException e){
+
+              }try{
+                boardA[r][c] += boardB[r][c+1];
+              }catch (IndexOutOfBoundsException e){
+
+              }try{
+                boardA[r][c] += boardB[r][c-1];
+              }catch (IndexOutOfBoundsException e){
+
+              }
+              /* shouldn't be like this, will do diagonals, do 4 seperate try catchs
+              for (int rC = 0; rC<2; rC++){
+                for (int cC = 0; cC<2; cC++){
+                  try{
+                    boardA[r][c]+=boardB[r+rC][c+cC];
+                  }catch (IndexOutOfBoundsException e){
+                    //don't do anything, means at edge
+                  }
+                }
+              }
+              */
+            }
+          }
+        }
+        toReturn = boardA[rowEnd][colEnd];
+      }else{ //modify boardB
+        System.out.println("\n" + curTime + "printing boardB");
+        //System.out.println(Arrays.toString(boardB));
+        for (int r = 0; r<rows; r++){
+          System.out.println(Arrays.toString(boardB[r]));
+          for (int c = 0; c<cols; c++){
+            if (boardA[r][c] == 0 && canUpdate[r][c]){ //means can be modified in other board
+              try{
+                boardB[r][c] += boardA[r+1][c];
+              }catch (IndexOutOfBoundsException e){
+
+              }try{
+                boardB[r][c] += boardA[r-1][c];
+              }catch (IndexOutOfBoundsException e){
+
+              }try{
+                boardB[r][c] += boardA[r][c+1];
+              }catch (IndexOutOfBoundsException e){
+
+              }try{
+                boardB[r][c] += boardA[r][c-1];
+              }catch (IndexOutOfBoundsException e){
+
+              }
+            }
+          }
+        }
+        toReturn = boardB[rowEnd][colEnd];
+      }
+    }
+    return toReturn;
   }
 }
